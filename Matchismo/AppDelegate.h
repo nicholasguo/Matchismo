f5f025a87a2a60914eb41b3da89490255eafa2dd
Nicholas Guo <nicholasguo@nicholass-mbp.attlocal.net>
2014-07-27 11:17:25 -0700

    Initial Commit

---
+++ Matchismo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Matchismo
+//
+//  Created by Nicholas Guo on 7/27/14.
+//  Copyright (c) 2014 Nicholas Guo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Matchismo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Matchismo
+//
+//  Created by Nicholas Guo on 7/27/14.
+//  Copyright (c) 2014 Nicholas Guo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
